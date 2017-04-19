2f9b79e20300364d25e49362590cbc3bcc9b1b89
JelmerVNuss <jelmervannuss@gmail.com>
2017-04-19 17:00:39 +0200

    Update plotting

--- StatisticsPlotter.py
+++ StatisticsPlotter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 
@@ -27,7 +28,14 @@ class StatisticsPlotter:
 
     def plotStatistics(self):
         fig, ax = plt.subplots()
+        ax.margins(x=0.05, y=0.01)
+
         x = [self.dataLabels.index(dataLabel) for dataLabel in self.dataLabels]
         y = self.means
         ax.errorbar(x, y, yerr=self.standardDeviations)
+        ax.set_xticklabels(self.dataLabels)
+
+        # Restrict the labels to integers only.
+        xa = ax.get_xaxis()
+        xa.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
         plt.show()
